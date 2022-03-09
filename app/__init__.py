292bb94caeac371c684efab8f101209140c73095
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-10 00:43:09 +0300

    Register the blueprints to the main app instance

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,15 @@ def create_app(config_name):
 
     app = Flask(__name__)
 
+    # Add App Configs
     app.config.from_object(config_options[config_name])
+
+    # Register App Blueprints
+    from .main.auth import auth
+    from .main.views import views
+
+    app.register_blueprint(views)
+    app.register_blueprint(auth, url_prefix='/auth') 
 
 
     return app

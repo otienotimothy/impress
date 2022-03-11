6b1d71f0ad1f4d3d36b068d8239762aaea3525cc
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-11 22:20:26 +0300

    Add flask migrate package to handle database migrations

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
+from enum import unique
 from . import db
 
 class User(db.Model):
-    id = db.column()
+    id = db.column(db.Integer, primary_key = True)
+    email = db.Column(db.String(150), unique = True)
+    password = db.Column(db.String(150))

b381535d1de6e59a5e358f97966e5d55174f84c6
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-12 11:45:54 +0300

    Testing other implementations of the migrations bug

---
+++ migrations/versions/432837312054_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 432837312054
+Revises: 
+Create Date: 2022-03-12 11:45:12.096572
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '432837312054'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

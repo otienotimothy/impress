efe64ea0506572cd4fa85cf245bd3166d9565c38
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-13 14:38:54 +0300

    Add a Pitch Model

--- app/models.py
+++ app/models.py
@@ -8,3 +8,14 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(150), unique = True)
     password = db.Column(db.String(150))
+    pitches = db.relationship('Pitch', backref = 'user', passive_deletes= True, lazy='dynamic')
+
+
+class Pitch(db.Model):
+
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key = True)
+    pitch = db.Column(db.Text, nullable = False)
+    pitch_category = db.Column(db.String(128))
+    author = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable = False )

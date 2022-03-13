274ea64aec28b9ca3668e8f32ea144a52d1c77ce
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-13 15:45:32 +0300

    Add data validation to add pitch form and fumctionality to add a new pitch to the database

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from crypt import methods
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, current_user
 from .forms import Create_pitch
+from app.models import Pitch
+from app import db
 
 views = Blueprint('views', __name__)
 
@@ -9,8 +12,22 @@ def index():
     return render_template('index.html')
 
 
-@views.route('/add_pitch')
+@views.route('/add_pitch', methods= ['GET', 'POST'])
 @login_required
 def add_pitch():
     pitch = Create_pitch()
+
+    if pitch.validate_on_submit():
+        pitch_category = request.form.get('pitch_category')
+        pitch_text = request.form.get('pitch')
+
+        pitch_item = Pitch(pitch_category=pitch_category, pitch=pitch_text, author= current_user.id)
+
+        db.session.add(pitch_item)
+        db.session.commit()
+
+        flash(f'Your Pitch has been added successfully.', 'success')
+
+        pitch.pitch.data = " "
+
     return render_template('add_pitch.html', title= 'Add Pitch', new_pitch = pitch)

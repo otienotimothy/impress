510167ad324c57d2bcd6aeaf730fb516b6790405
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-13 16:34:53 +0300

    Add a username to the user model class

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class User(UserMixin,db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(150), unique = True)
+    username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     pitches = db.relationship('Pitch', backref = 'user', passive_deletes= True, lazy='dynamic')
 

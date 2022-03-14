c32acd0a38a55d3e681c9fc6ea01644d91a53eb0
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-03-14 08:03:53 +0300

    Modify the config file to receive database uri from environment variable

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:moringa@localhost/impress'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class DevConfig(Config):
     DEBUG = True
